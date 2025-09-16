8532837b0ae7fb772684efc7eb3584bb8ccf4046
danibocchino <daniel.bocchino@gmail.com>
2025-09-15 21:48:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="p-6">
+      <h1 className="text-2xl font-bold">Income & Debt Tracker</h1>
+      <p className="text-gray-600">Welcome Debi & Bocha! 🚀</p>
+    </div>
+  )
+}
+
+export default App
